3d9db323b5fc5104f3af41d11feeeff8254fb020
LaaaXeD <Butlerlj@ymail.com>
2026-02-26 01:35:25 +0000

    Adding Aritmetic

--- arithmetic.c
+++ arithmetic.c
@@ -12,34 +12,38 @@
     
     */
 int main(){
+    // Initial Values
+    int x = 10;
+    int y = 3;
+    int z = 0;
 
-int x = 10;
-int y = 2;
-int z = 0;
+    // Addition
+    z = x + y;
 
-z = x + y; // Addition
-printf("Addition: %d\n", z);
+    // printf("%f\n", z);
+    
+    // Division
+    // Remember that if you divide two integers, the result will be an integer. 
+    // If you want a float result, at least one of the operands must be a float.
+    z = x / y;
 
-z = y - x; // Subtraction
-printf("Subtraction: %d\n", z);
+    printf("%d \n", z);
 
-z = x * y; // Multiplication
-printf("Multiplication: %d\n", z);
+    // Modulus
+    z = x % y;
 
-z = x / y; // Division
-// Keep in mind you can't get a decimal if the variables are both integers there will be a problem
-// If both variables are converted to floats this will fix the issue
-printf("Division: %f\n", z);
+    printf("%d \n", z);
 
-// Modulus
-z = x % y; 
-printf("Modulus: %d\n", z);
+    // Increment and Decrement
+    x++; // Increment x by 1
+    y--; // Decrement y by 1
 
-// ++ Increment
-x++;
-printf("Increment: %d\n", x);
-
-
+    // Augmnent Assignment Operators
+    x += 5; // Equivalent to x = x + 5
+    y *= 2; // Equivalent to y = y * 2
+    printf("%d %d\n", x, y);
 
 
+
+    return 0;
 }
