504c911371e2e7a6f795cdc3bb722f3ea25872d8
Lj <Butlerlj@ymail.com>
2026-03-15 22:29:31 +0000

    Adding arrays file.

---
+++ arrays.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // array = a data structure that can store a fixed number of values of the same type.
+
+    double prices[] = {5.0,10.0,15.0,20.0}; // Declare and initialize an array of doubles
+    
+    printf("$%.2lf", prices[0]); // Print the first element of the array
+
+
+    return 0;
+}
